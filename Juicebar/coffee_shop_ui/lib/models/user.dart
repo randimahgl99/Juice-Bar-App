e8878edd11a672d6654dedf267c9402908498655
randimahgl99 <randimahgl99@gmail.com>
2024-12-03 12:48:37 +0530

    add new user

--- Juicebar/coffee_shop_ui/lib/models/user.dart
+++ Juicebar/coffee_shop_ui/lib/models/user.dart
@@ -3,16 +3,11 @@ class User {
   final String email;
   final String password;
 
-  User({
-    required this.userName,
-    required this.email,
-    required this.password
-  });
+  User({required this.userName, required this.email, required this.password});
 
-    static List<User> userList = [
-     User(userName: 'lahiru', email: 'lahiru@gmail.com', password: '12345678'),
-     User(userName: 'athila', email: 'athila@gmail.com', password: '12345678'),
-   
+  static List<User> userList = [
+    User(userName: 'lahiru', email: 'lahiru@gmail.com', password: '12345678'),
+    User(userName: 'athila', email: 'athila@gmail.com', password: '12345678'),
+    User(userName: 'test', email: 'test@gmail.com', password: '12345678'),
   ];
 }
-
